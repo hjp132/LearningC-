5b483e2b5777dc29e701bbf10b4a9e55de54da90
Harry Palmer <harry.palmer@crafted.co.uk>
2021-08-20 09:27:03 +0100

    Learning C# MVC
    Pluralsight tutorial & Microsoft tutorial

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pluralsightCourse = new Course();
+            pluralsightCourse.Name = "Working with C# Records";
+            pluralsightCourse.Author = "Roland Guijt";
+
+            var anotherCourse = pluralsightCourse;
+            Console.WriteLine(pluralsightCourse.Name);
+            Console.WriteLine(pluralsightCourse.Author);
+
+            var classroomCourse = new CourseRecord(
+                "Working with C# Records",
+                "Roland Guijt"
+                );
+
+
+            Console.ReadLine();
+        }
+    }
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        public void Deconstruct(out string name, out string author)
+        {
+            name = Name;
+            author = Author;
+        }
+    }
+
+    public record CourseRecord(string Name, string Author);
+    
+}
